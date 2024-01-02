cc0be0841c914f3d2beb707c6652c14315a8193e
Ioana Iosub <stu.waltham@gmail.com>
2024-01-02 18:08:58 +0100

    Display Answer if Correct or Incorrect

--- assets/questions.js
+++ assets/questions.js
@@ -2,13 +2,18 @@
 const questions = [
   {
     question: "Where is the correct place to insert a JavaScript?",
-    choices: ["head section", "body section", "", "head and body sections"],
+    choices: [
+      "head section",
+      "body section",
+      "head and body sections",
+      "none of this",
+    ],
     correctAnswer: 1,
   },
   {
     question:
       " ____ is a keyword used to declare a variable that cannot be reassigned a new value.",
-    choices: ["const", "var", "let"],
+    choices: ["const", "var", "let", "for"],
     correctAnswer: 0,
   },
   {
@@ -17,6 +22,7 @@ const questions = [
       "function functionName()",
       "function:functionName()",
       "functionName",
+      "function",
     ],
     correctAnswer: 0,
   },
@@ -27,6 +33,7 @@ const questions = [
       "for (i<=5)",
       "for i= 1 to 5",
       "for(let i=0; i >5;i++)",
+      "for= i++",
     ],
     correctAnswer: 3,
   },
@@ -36,11 +43,12 @@ const questions = [
     correctAnswer: 2,
   },
   {
-    question: "How can you add a comment in a JavaScript?",
+    question: "How can you add a single line comment in a JavaScript?",
     choices: [
       "<-This is a comment->",
       "//This is a comment",
       "'This is a comment",
+      "/*This is a comment",
     ],
     correctAnswer: 1,
   },
@@ -58,7 +66,6 @@ function startQuiz() {
   let currentQuestionIndex = 0;
   const questionTitle = document.getElementById("question-title");
   const questionsDiv = document.getElementById("questions");
-  const choices = document.getElementById("choices");
   const currentQuestion = questions[currentQuestionIndex];
 
   questionTitle.textContent = currentQuestion.question;
@@ -66,6 +73,32 @@ function startQuiz() {
 
   const startScreen = document.querySelector("#start-screen");
   startScreen.classList.add("hide");
+
+  const choicesContainer = document.getElementById("choices");
+  const ulElement = document.createElement("ol");
+  const firstQuestionChoices = currentQuestion.choices;
+
+  firstQuestionChoices.forEach((choiceText, index) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = choiceText;
+    ulElement.appendChild(liElement);
+
+    liElement.addEventListener("click", () => {
+      const barLine = document.createElement("hr");
+      questionsDiv.appendChild(barLine);
+
+      const answer = document.createElement("p");
+      questionsDiv.appendChild(answer);
+
+      if (index === currentQuestion.correctAnswer) {
+        answer.textContent = "Correct!";
+      } else {
+        answer.textContent = "Incorect!";
+      }
+    });
+  });
+
+  choicesContainer.appendChild(ulElement);
 }
 
 const startButton = document.getElementById("start");
